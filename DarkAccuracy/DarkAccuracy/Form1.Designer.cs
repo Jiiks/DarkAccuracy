059e419fd48b8bcdf5a295fd10c956c856b733de
jiiks <jiiks@windowslive.com>
2015-02-08 19:30:09 +0200

    init

---
+++ DarkAccuracy/DarkAccuracy/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Critter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.logBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 70);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Instructions";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(242, 51);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Move this executable inside:\nSteamApps\\common\\DarkestDungeon\\\nPress the button";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 88);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(248, 43);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Warning";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(3, 16);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(242, 24);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "BACKUP EVERYTHING, THERE IS NO CTRL-Z";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GIT GUD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // logBox
+            // 
+            this.logBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logBox.Location = new System.Drawing.Point(3, 16);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(309, 126);
+            this.logBox.TabIndex = 3;
+            this.logBox.Text = "";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.logBox);
+            this.groupBox3.Location = new System.Drawing.Point(266, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(315, 145);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Log";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 165);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "100% Accuracy";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox logBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ DarkAccuracy/DarkAccuracy/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Critter
+{
+
+
+    public partial class Form1 : Form
+    {
+        private String[] classesArray = { "bounty_hunter", "crusader", "grave_robber", "hellion", "highwayman", "jester", "leper", "occultist", "plague_doctor", "vestal" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var confirmResult = MessageBox.Show("Did you backup everything??!?!?",
+                                     "Time to win!!",
+                                     MessageBoxButtons.YesNo);
+            if (confirmResult != DialogResult.Yes) return;
+            var confirmResult2 = MessageBox.Show("Did you really backup everything??!?!?",
+                "Time to win!!",
+                MessageBoxButtons.YesNo);
+            if (confirmResult2 != DialogResult.Yes) return;
+            var confirmResult3 = MessageBox.Show("The whole heroes folder??!?!?",
+                "Time to win!!",
+                MessageBoxButtons.YesNo);
+            if (confirmResult3 != DialogResult.Yes) return;
+
+            append("Starting");
+
+            foreach (string s in classesArray)
+            {
+                String file = Application.StartupPath + "\\heroes\\" + s + "\\" + s + ".info.darkest";
+                if (!File.Exists(file))
+                {
+                    MessageBox.Show("File: " + file + " not found!");
+                    return;
+                }
+                append(s + " info file located");
+            }
+
+
+            //Magic
+            foreach (string s in classesArray)
+            {
+                String file = Application.StartupPath + "\\heroes\\" + s + "\\" + s + ".info.darkest";
+                append("Processing " + file);
+                File.WriteAllText(file, Regex.Replace(File.ReadAllText(file), ".atk (.*?)\\w%", ".atk 100%"));
+                append("Processed " + file);
+            }
+
+
+            String path = Application.StartupPath + "\\monsters";
+
+            var monsterConfirm = MessageBox.Show("Do you want monsters to get 100% accuracy aswell?", "Mobs",
+                MessageBoxButtons.YesNo);
+
+            if (monsterConfirm == DialogResult.Yes)
+            {
+                var monsterConfirm2 = MessageBox.Show("Did you backup the monsters folder!??!?!?", "Mobs",
+                    MessageBoxButtons.YesNo);
+
+                if (monsterConfirm2 == DialogResult.Yes)
+                {
+
+                    foreach (String s in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
+                    {
+                        if (s.Contains("info.darkest"))
+                        {
+                            append("Processing " + s);
+                            File.WriteAllText(s, Regex.Replace(File.ReadAllText(s), ".atk (.*?)\\w%", ".atk 100%"));
+                            append("Processed " + s);
+                        }
+                    }
+                }
+            }
+
+
+            MessageBox.Show("All Done!");
+        }
+
+        private void append(String text)
+        {
+            logBox.AppendText(text + "\n");
+            logBox.ScrollToCaret();
+        }
+    }
+}
